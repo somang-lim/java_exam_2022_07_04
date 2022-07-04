285097c09148387a8916502bad5d83aa3f5ff762
somang-lim <somangjuha@gmail.com>
2022-07-04 15:37:24 +0900

    더하기 기능 구현

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -4,9 +4,10 @@ public class Calculator {
 
 
     public static int add(String s) {
-        if(s.equals("20 + 20")) {
-            return 40;
-        }
-        return 30;
+        String[] sBits = s.split(" \\+ ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 + no2;
     }
 }
