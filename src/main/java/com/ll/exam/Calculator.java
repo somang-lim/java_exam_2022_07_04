47d1bdfe85d3957d4f4c6bf8536d09d66d4f2e9e
somang-lim <somangjuha@gmail.com>
2022-07-05 11:25:09 +0900

    빼기 기능 구현

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -2,12 +2,31 @@ package com.ll.exam;
 
 public class Calculator {
 
+    public static int run(String s) {
+        // -가 들어있으면 index 숫자를 반환한다. 없다면 -1을 반환한다.
+        boolean isMinus = s.indexOf(" - ") != -1;
 
-    public static int add(String s) {
+        if(isMinus) {
+            return runMinus(s);
+        }
+
+        return runPlus(s);
+    }
+
+    private static int runPlus(String s) {
         String[] sBits = s.split(" \\+ ");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
 
         return no1 + no2;
     }
+
+    private static int runMinus(String s) {
+        String[] sBits = s.split(" \\- ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 - no2;
+    }
+
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -7,24 +7,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
 
     @Test // 컴퓨터가 읽는 주석
-    public void 더하기_테스트() {
-        int rs = Calculator.add("10 + 20");
+    public void 더하기() {
+        int rs = Calculator.run("10 + 20");
 
         // expected : 기대한(예상한) 값
         assertEquals(30, rs);
     }
 
     @Test
-    public void 더하기_테스트_2() {
-        int rs = Calculator.add("20 + 20");
+    public void 더하기_2() {
+        int rs = Calculator.run("20 + 20");
 
         assertEquals(40, rs);
     }
 
     @Test
-    public void 더하기_테스트_3() {
-        int rs = Calculator.add("10 + 10");
+    public void 더하기_3() {
+        int rs = Calculator.run("10 + 10");
 
         assertEquals(20, rs);
+    }
+    
+    @Test
+    public void 빼기() {
+        int rs = Calculator.run("10 - 10");
+
+        assertEquals(0, rs);
     }
 }
